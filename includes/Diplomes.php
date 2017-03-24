999ea1e3e876d9869b5376eab83e55b473912c5b
Gabriel <gabriel.noiret@gmail.com>
2017-03-24 18:11:33 +0100

    Sitede base

---
+++ includes/Diplomes.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Diplomes</title>
+	</head>
+	<body>
+		<div id="Diplomes">
+			<fieldset>
+				<legend><h2>Mes diplomes</h2></legend>
+				<ul>
+					<li>Licence Porfessionnelle : Aménagement du térritoire et urbanisme: spécialité cartographie, topographie, Systemes d'information Géographique <br>
+					<em>Université d'Orléans (45)</em></li>
+					<li>Brevet Technicien Supérieur Géomètre Topographe <br>
+					<em>Lycée Gaudier-Brzeska - Saint-Jean-de-Braye (45)</em></li>
+					<li>Baccalauréat professionnel Technicien Géomètre Topographe <br>
+					<em>Lycée Pierre Joël Bonté - Riom (63)</em></li>
+					<li>Brevet d'études professionnelles Technicien Géomètre Topographe <br>
+					<em>Lycée Pierre Joël Bonté - Riom (63)</em></li>
+				</ul>
+			</fieldset>
+		</div>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang = "fr">
+	<head><!--Les métadoonnées-->
+		<meta charset="utf-8">
+	    <title>Gabriel NOIRET</title>
+
+	   	<link rel="stylesheet" type="text/css" href="style.css"><!-- Page CSS (style) -->
+	   	<!-- <link rel="stylesheet" type="text/css" href="style/bootstrap/css/bootstrap.min.css"> -->
+	   	<meta name="viewport" content="width=device-width" />
+	  	<meta name="description" content="Mom CV WEB Géomaticien Cartographe Gabriel NOIRET">
+	  	<meta name="keywords" content="Géomaticien ENSG  - Géomètre">
+	  	<meta name="author" content="Gabriel NOIRET"><!-- Auteur du site -->
+	  	<link rel="shortcut icon" type="image/x-icon" href="Images/mini_Logo.png" /><!-- Image onglet -->
+	  	<link rel="apple-touch-icon-precomposed" href="Images/mini_Logo.png" />
+	  	<link rel="apple-touch-icon" href="Images/mini_Logo.png" />
+	  	<link rel="apple-touch-icon" sizes="72x72" href="Images/mini_Logo.png" />
+	</head>
+	<body> <!--Corp de la page-->
+		<div id= "menu_bas">
+			<?php include 'includes/nav.php'; ?>	
+		</div>
+
+		<div id = "menu" >
+			<header>
+				<?php include 'includes/header.php'; ?>
+			</header>
+
+			<nav id="test">
+				<?php include 'includes/nav.php'; ?>
+			</nav>
+
+			<footer>
+				<?php include 'includes/footer.php'; ?>
+			</footer>
+		</div>
+		
+		<div id= "page">
+
+			<section id="Lien_1"><?php include 'includes/Lien_1.php'; ?></section>
+
+			<section id="tp_webmap"><?php include 'includes/tp_webmap.php'; ?></section>
+
+			<section id="Formations"><?php include 'includes/Formations.php'; ?></section>
+
+			<section id="Diplomes"><?php include 'includes/Diplomes.php'; ?></section>
+
+			<section id="Compétences"><?php include 'includes/Competences.php'; ?></section>
+
+			<section id="Expériences"><?php include 'includes/Experiences.php'; ?></section>
+
+			<section id="Autres"><?php include 'includes/Autres.php'; ?></section>
+		</div>
+	</body>
+</html>
